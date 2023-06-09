75673e741396f56421c587a887a333e330dd7604
AlanChiangg <alaln35alan@gmail.com>
2023-06-10 00:12:57 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')  // 載入mongoose
 const exphbs = require('express-handlebars');
 
+const Todo = require('./models/todo')
+
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -26,7 +28,11 @@ db.once('open', () => {
 })
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // 拿到全部的Todo資料
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.listen(3000, () => {
