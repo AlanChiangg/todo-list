57d034582ef6405b7b5b28d7cdf4935ea7b5e0f5
AlanChiangg <alaln35alan@gmail.com>
2023-06-10 16:04:12 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const mongoose = require('mongoose')  // 載入mongoose
+const mongoose = require('mongoose')  // 載入 mongoose
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser') // 載入 body-parser
 
-const Todo = require('./models/todo')
+const Todo = require('./models/todo');
+const { redirect } = require('express/lib/response');
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
 if (process.env.NODE_ENV !== 'production') {
@@ -27,12 +29,26 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   // 拿到全部的Todo資料
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
+})
+
+app.get('/todos/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name        // 從 req.body 拿出表單的 name 資料
+  return Todo.create({ name })      // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後,導回首頁
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
