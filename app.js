8668a8e56d889b8420229b6fc8ff4ab5978ad562
AlanChiangg <alaln35alan@gmail.com>
2023-06-12 12:52:34 +0800

    feat: add sort function

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.get('/', (req, res) => {
   // 拿到全部的Todo資料
   Todo.find() // 取出 Todo model 裡的所有資料
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .sort({ _id: 'asc' }) //正序排列,  'desc'為相反
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
