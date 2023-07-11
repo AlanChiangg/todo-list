195e439584c80be5062cb8244d7ac9ff6e68837b
AlanChiangg <alaln35alan@gmail.com>
2023-07-11 16:50:35 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const home = require('./modules/home') 
-const todos = require('./modules/todos')  
+const home = require('./modules/home')
+const todos = require('./modules/todos')
 const users = require('./modules/users')
 
-router.use('/', home)  
-router.use('/todos', todos)  
+const { authenticator } = require('../middleware/auth')
+
 router.use('/users', users)
+router.use('/todos', authenticator, todos)
+router.use('/', authenticator, home)
 
 module.exports = router
