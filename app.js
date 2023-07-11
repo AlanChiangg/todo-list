3f072add3c436c9ed00bdbcc719982bbc2e4f255
AlanChiangg <alaln35alan@gmail.com>
2023-07-11 12:03:39 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser') // 載入 body-parser
 const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3000
 
-const routes = require('./routes')  // 引用路由器
-require ('./config/mongoose')
+const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-// 用 app.use 規定每筆請求都需先經過以下前置處理
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-app.use(routes)  // 將 request 導入路由器
+app.use(routes)
 
 
 app.listen(PORT, () => {
